f54d3d41c7db4329345f1025f9bc522619ecadb1
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-03-01 14:06:21 +0100

    feat(refactor): Java refactoring of User and Investment

--- src/Investment.java
+++ src/Investment.java
@@ -1,2 +1,86 @@
 public class Investment {
+    private double amount;
+    private double increasedRate;
+    private int duration;
+    private int profitRisk;
+    private int currentMonth;
+    private double currentGainedMoney;
+    private boolean status;
+    private String startingDate;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getIncreasedRate() {
+        return increasedRate;
+    }
+
+    public void setIncreasedRate(double increasedRate) {
+        this.increasedRate = increasedRate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getProfitRisk() {
+        return profitRisk;
+    }
+
+    public void setProfitRisk(int profitRisk) {
+        this.profitRisk = profitRisk;
+    }
+
+    public int getCurrentMonth() {
+        return currentMonth;
+    }
+
+    public void setCurrentMonth(int currentMonth) {
+        this.currentMonth = currentMonth;
+    }
+
+    public double getCurrentGainedMoney() {
+        return currentGainedMoney;
+    }
+
+    public void setCurrentGainedMoney(double currentGainedMoney) {
+        this.currentGainedMoney = currentGainedMoney;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(String startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public Investment(double amount, double increasedRate, int duration, int profitRisk, String startingDate) {
+        this.amount = amount;
+        this.increasedRate = increasedRate;
+        this.duration = duration;
+        this.profitRisk = profitRisk;
+        this.startingDate = startingDate;
+
+        this.currentGainedMoney = amount;
+        this.currentMonth = 0;
+        this.status = true;
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,165 @@
+import java.util.Vector;
+
 public class User {
+    private final String username;
+    private double walletMoney;
+    private double bankBalance;
+    private Vector<Investment> investmentsList;
+
+    public User(String username, double walletMoney) {
+        this(username, walletMoney, 0);
+    }
+
+    public User(String username, double walletMoney, double bankBalance) {
+        this.username = username;
+        this.walletMoney = walletMoney;
+        this.bankBalance = bankBalance;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public double getBankBalance() {
+        return this.bankBalance;
+    }
+
+    public double getWalletMoney() {
+        return this.walletMoney;
+    }
+
+    public void addMoneyToBalance(double money) {
+        this.bankBalance += money;
+        this.walletMoney -= money;
+    }
+
+    public void withdrawMoneyFromBalance(double money) {
+        this.bankBalance -= money;
+        this.walletMoney += money;
+    }
+
+    public void addMoneyWallet(double money) {
+        this.walletMoney += money;
+    }
+
+    public void addInvestment(double amount, int increasedRate, int duration, int profitRisk, String startingDate) {
+        Investment tempInvestment = new Investment(amount, increasedRate, duration, profitRisk, startingDate);
+        investmentsList.add(tempInvestment);
+    }
+
+    public void removeMoneyBalance(double money) {
+        this.bankBalance -= money;
+    }
+
+    public void printInvestments() {
+        StringBuilder tempInvestment = new StringBuilder();
+        for (int i = investmentsList.size() - 1; i >= 0; i--) {
+            // tempInvestment.clear();
+
+            tempInvestment.append(investmentsList.get(i).getStartingDate());
+            tempInvestment.append(" | ");
+            tempInvestment.append((investmentsList.get(i).getStatus()) ? "IN CORSO" : "FINITO");
+            tempInvestment.append(" | ");
+            tempInvestment.append(investmentsList.get(i).getAmount());
+
+            // ?
+            // tempInvestment.toString().erase(tempInvestment.length() - 4, 4);
+
+            tempInvestment.append('$');
+            tempInvestment.append(" | ");
+            tempInvestment.append(investmentsList.get(i).getDuration());
+            tempInvestment.append(" | ");
+            tempInvestment.append(investmentsList.get(i).getIncreasedRate());
+
+            // ?
+            // tempInvestment.toString().erase(tempInvestment.length() - 4, 4);
+
+            tempInvestment.append('%');
+            tempInvestment.append(" | ");
+
+            if (investmentsList.get(i).getProfitRisk() == 1) {
+                tempInvestment.append("BASSO RISCHIO");
+            } else if (investmentsList.get(i).getProfitRisk() == 2) {
+                tempInvestment.append("MEDIO RISCHIO");
+            } else {
+                tempInvestment.append("ALTO RISCHIO");
+            }
+
+            tempInvestment.append(" | ");
+            tempInvestment.append(investmentsList.get(i).getCurrentGainedMoney());
+
+            // ?
+            // tempInvestment.toString().erase(tempInvestment.length() - 4, 4);
+
+            tempInvestment.append('$');
+
+            System.out.println(tempInvestment.toString());
+            System.out.println();
+        }
+    }
+
+    public void manageInvestments() {
+        double money;
+
+        for (int i = 0; i < investmentsList.size(); i++) {
+            if (investmentsList.get(i).getStatus()) {
+                money = investmentsList.get(i).getCurrentGainedMoney();
+
+                switch (investmentsList.get(i).getProfitRisk()) {
+                    case 1:
+                        investmentsList.get(i).setIncreasedRate(Math.random() % 5 + 1);
+
+                        if (Math.random() % 11 > 8) {
+                            investmentsList.get(i).setIncreasedRate(investmentsList.get(i).getIncreasedRate() * -1);
+                        }
+                        break;
+                    case 2:
+                        investmentsList.get(i).setIncreasedRate(Math.random() % 10 + 6);
+
+                        if (Math.random() % 11 > 6) {
+                            investmentsList.get(i).setIncreasedRate(investmentsList.get(i).getIncreasedRate() * -1);
+                        }
+
+                        break;
+                    case 3:
+                        investmentsList.get(i).setIncreasedRate(Math.random() % 10 + 16);
+                        if (Math.random() % 7 == 4) {
+                            if (Math.random() % 11 > 4) {
+                                investmentsList.get(i).setIncreasedRate(Math.random() % 50 + 101);
+                            } else {
+                                investmentsList.get(i).setIncreasedRate((Math.random() % 50 + 101) * -1);
+                            }
+                        } else {
+                            investmentsList.get(i).setIncreasedRate(Math.random() % 10 + 16);
+
+                            if (Math.random() % 11 > 4) {
+                                investmentsList.get(i).setIncreasedRate(investmentsList.get(i).getIncreasedRate() * -1);
+                            }
+                        }
+
+                        break;
+                }
+
+                money = investmentsList.get(i).getIncreasedRate() / 100 * Math.abs(money);
+
+                investmentsList.get(i).setCurrentGainedMoney(investmentsList.get(i).getCurrentGainedMoney() + money);
+                investmentsList.get(i).setCurrentMonth(investmentsList.get(i).getCurrentMonth() + 1);
+                if (investmentsList.get(i).getCurrentMonth() == investmentsList.get(i).getDuration()) {
+                    bankBalance += investmentsList.get(i).getCurrentGainedMoney();
+                    investmentsList.get(i).setStatus(false);
+                }
+            }
+        }
+    }
+
+    // Can be private?
+    public boolean checkStatusInvestments() {
+        for (int i = 0; i < investmentsList.size(); i++) {
+            if (investmentsList.get(i).getStatus()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
