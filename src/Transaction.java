b725359b6798ae8408bb6974bb2c134982461ed8
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-03-14 18:32:08 +0100

    feat!: date variable for keeping track of Dates

--- src/Transaction.java
+++ src/Transaction.java
@@ -5,18 +5,21 @@ public class Transaction {
     private final User receiver;
     private final double amount;
     private final String action;
+    private final String date;
 
-    public Transaction(User sender, User receiver, double amount) {
-        this(sender, receiver, amount, "");
+    public Transaction(User sender, User receiver, double amount, String date) {
+        this(sender, receiver, amount, "", "date");
     }
 
-    public Transaction(User sender, User receiver, double amount, String action) {
+    public Transaction(User sender, User receiver, double amount, String action,
+                       String date) {
         // A User gets deep copied at the moment of a Transaction to be able to see
         // and access values that might have changed such as walletMoney or bankBalance
         this.sender = new User(sender.returnObject());
         this.receiver = new User(receiver.returnObject());
         this.amount = amount;
         this.action = action;
+        this.date = date;
     }
 
     public Transaction(JSONObject object) {
@@ -24,6 +27,7 @@ public class Transaction {
         this.receiver = new User((JSONObject) object.get("sender"));
         this.amount = (double) object.get("amount");
         this.action = object.get("action").toString();
+        this.date = object.get("date").toString();
     }
 
     public String getSender() {
@@ -42,6 +46,10 @@ public class Transaction {
         return this.action;
     }
 
+    public String getDate() {
+        return this.date;
+    }
+
     public JSONObject returnObject() {
         JSONObject transaction = new JSONObject();
 
@@ -49,13 +57,15 @@ public class Transaction {
         transaction.put("receiver", this.receiver.returnObject());
         transaction.put("amount", this.amount);
         transaction.put("action", this.action);
+        transaction.put("date", this.date);
 
         return transaction;
     }
 
     @Override
     public String toString() {
-        return (this.getSender() + " --> " + this.getReceiver() + "\n"
-                + this.getAction() + "   " + this.getAmount() + " €");
+        return (this.getDate() + "\n"
+                + " " + this.getSender() + " --> " + this.getReceiver() + "\n"
+                + " " + this.getAction() + "   " + this.getAmount() + " €\n");
     }
 }
