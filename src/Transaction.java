b24d99ee79d9826854c00941fbee0501aecc2138
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-03-14 17:48:13 +0100

    feat: JSONObject Constructor and returnObject

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,3 +1,5 @@
+import org.json.simple.JSONObject;
+
 public class Transaction {
     private final User sender;
     private final User receiver;
@@ -9,10 +11,19 @@ public class Transaction {
     }
 
     public Transaction(User sender, User receiver, double amount, String action) {
-        this.sender = sender;
-        this.receiver = receiver;
+        // A User gets deep copied at the moment of a Transaction to be able to see
+        // and access values that might have changed such as walletMoney or bankBalance
+        this.sender = new User(sender.returnObject());
+        this.receiver = new User(receiver.returnObject());
         this.amount = amount;
         this.action = action;
+    }
+
+    public Transaction(JSONObject object) {
+        this.sender = new User((JSONObject) object.get("sender"));
+        this.receiver = new User((JSONObject) object.get("sender"));
+        this.amount = (double) object.get("amount");
+        this.action = object.get("action").toString();
     }
 
     public String getSender() {
@@ -29,6 +40,17 @@ public class Transaction {
 
     public String getAction() {
         return this.action;
+    }
+
+    public JSONObject returnObject() {
+        JSONObject transaction = new JSONObject();
+
+        transaction.put("sender", this.sender.returnObject());
+        transaction.put("receiver", this.receiver.returnObject());
+        transaction.put("amount", this.amount);
+        transaction.put("action", this.action);
+
+        return transaction;
     }
 
     @Override
